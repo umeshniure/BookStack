e6f6ee0875bf01724de8c452486a751b6c9242be
umeshniure <70754744+umeshniure@users.noreply.github.com>
2022-11-06 14:29:37 +0545

    database updated

--- build/web/WEB-INF/classes/com/model/bookstack_db.sql
+++ build/web/WEB-INF/classes/com/model/bookstack_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 26, 2022 at 07:32 PM
+-- Generation Time: Nov 06, 2022 at 09:43 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -51,11 +51,11 @@ CREATE TABLE `books` (
 --
 
 INSERT INTO `books` (`id`, `name`, `isbn`, `author`, `published_year`, `category`, `price`, `cover_photo`, `publication`, `discounted_price`, `description`, `cover_type`, `language`, `type`, `cover_photo_name`, `vendor_id`) VALUES
-(4, 'Do Epic Shit', 2663522, 'Ankur Warikoo', 2021, 6, 500, 'C:\\Users\\Umesh\\OneDrive\\Documents\\NetBeansProjects\\BookStack\\web\\images\\book_cover_photos\\3\\Do Epic Shit-vendor3.png', 'Mumbai Publishers', NULL, NULL, 1, 1, 1, 'Do Epic Shit-vendor3.png', 3),
-(5, 'The Alchemist', 43563456455645, 'paulo coehaelo', 2002, 4, 450, 'C:\\Users\\Umesh\\OneDrive\\Documents\\NetBeansProjects\\BookStack\\web\\images\\book_cover_photos\\4\\The Alchemistt-vendor4.png', 'harper collins', 410, 'dfbdfg egbgb dfgb fg fghdfgh herth eth erth eerth rth ', 1, 2, 1, 'The Alchemistt-vendor4.png', 4),
+(4, 'Do Epic Shit', 2663523, 'Ankur Warikoo', 2021, 5, 500, 'C:\\Users\\Umesh\\OneDrive\\Documents\\NetBeansProjects\\BookStack\\web\\images\\book_cover_photos\\3\\Do Epic Shit-vendor3.png', 'Mumbai Publishers', 479, 'sredtyftughuijoojihughfds hbnm', 1, 1, 1, 'Do Epic Shit-vendor3.png', 3),
+(5, 'The Alchemist', 43563456455645, 'paulo coehaelo', 2002, 4, 450, 'C:\\Users\\Umesh\\OneDrive\\Documents\\NetBeansProjects\\BookStack\\web\\images\\book_cover_photos\\4\\The Alchemistt-vendor4.png', 'harper collins', NULL, 'dfbdfg egbgb dfgb fg fghdfgh herth eth erth eerth rth ', 1, 2, 1, 'The Alchemistt-vendor4.png', 4),
 (7, 'Rockin Rollin Rolpa', 232323234234, 'mahendra Joshi', 2018, 3, 400, 'C:\\Users\\Umesh\\OneDrive\\Documents\\NetBeansProjects\\BookStack\\web\\images\\book_cover_photos\\4\\Rockin Rollin Rolpa-vendor4.png', 'brosis india Pvt. Ltd.', NULL, 'zdfvsdfbsfd this a awesone rockin rollin book with tons of humor, drama, and culture heighlights.', 1, 1, 1, 'Rockin Rollin Rolpa-vendor4.png', 4),
 (8, 'The Subtle Art of Not Giving a Fuck', 4534523533462456, 'Mark Manson', 2016, 6, 550, 'C:\\Users\\Umesh\\OneDrive\\Documents\\NetBeansProjects\\BookStack\\web\\images\\book_cover_photos\\3\\The Subtle Art of Not Giving a Fuck-vendor3.png', 'Harper One Publishers', NULL, 'In this generation-defining self-help guide, a superstar blogger cuts through the crap to show us how to stop trying to be \"positive\" all the time so that we can truly become better, happier people.\r\n\r\nFor decades, weve been told that positive thinking is the key to a happy, rich life. \"F**k positivity,\" Mark Manson says. \"Lets be honest, shit is f**ked and we have to live with it.\" In his wildly popular Internet blog, Manson doesnt sugarcoat or equivocate. He tells it like it isa dose of raw, refreshing, honest truth that is sorely lacking today. The Subtle Art of Not Giving a F**k is his antidote to the coddling, lets-all-feel-good mindset that has infected modern society and spoiled a generation, rewarding them with gold medals just for showing up.\r\n\r\nManson makes the argument, backed both by academic research and well-timed poop jokes, that improving our lives hinges not on our ability to turn lemons into lemonade, but on learning to stomach lemons better. Human beings are flawed and limited\"not everybody can be extraordinary, there are winners and losers in society, and some of it is not fair or your fault.\" Manson advises us to get to know our limitations and accept them. Once we embrace our fears, faults, and uncertainties, once we stop running and avoiding and start confronting painful truths, we can begin to find the courage, perseverance, honesty, responsibility, curiosity, and forgiveness we seek.\r\n\r\nThere are only so many things we can give a f**k about so we need to figure out which ones really matter, Manson makes clear. While money is nice, caring about what you do with your life is better, because true wealth is about experience. A much-needed grab-you-by-the-shoulders-and-look-you-in-the-eye moment of real-talk, filled with entertaining stories and profane, ruthless humor, The Subtle Art of Not Giving a F**k is a refreshing slap for a generation to help them lead contented, grounded lives.', 2, 2, 1, 'The Subtle Art of Not Giving a Fuck-vendor3.png', 3),
-(9, 'The Subtle Art of Not Giving a Fuck', 3245345234, 'Mark Manson', 2016, 6, 610, 'C:\\Users\\Umesh\\OneDrive\\Documents\\NetBeansProjects\\BookStack\\web\\images\\book_cover_photos\\4\\The Subtle Art of Not Giving a Fuck-vendor4.png', 'Harper One Publishers', NULL, 'sdfvsdfbsdb sdfdfv sdfgw dffgwe rgwer gwerg wrgwe rgwerg werrg', 2, 2, 1, 'The Subtle Art of Not Giving a Fuck-vendor4.png', 4),
+(9, 'The Subtle Art of Not Giving a Fuck', 3245345234, 'Mark Manson', 2016, 6, 610, 'C:\\Users\\Umesh\\OneDrive\\Documents\\NetBeansProjects\\BookStack\\web\\images\\book_cover_photos\\4\\The Subtle Art of Not Giving a Fuck-vendor4.png', 'Harper One Publishers', 600, 'sdfvsdfbsdb sdfdfv sdfgw dffgwe rgwer gwerg wrgwe rgwerg werrg', 2, 2, 1, 'The Subtle Art of Not Giving a Fuck-vendor4.png', 4),
 (16, 'Rich Dad Poor Dad', 4534634, 'Robert kiyosakii', 2002, 5, 345, 'C:\\Users\\Umesh\\OneDrive\\Documents\\NetBeansProjects\\BookStack\\web\\images\\book_cover_photos\\4\\Rich Dad Poor Dad-vendor4.png', 'Harper One Publishers', NULL, 'dfvsdfs sdfvdsfgb', 2, 2, 1, 'Rich Dad Poor Dad-vendor4.png', 4);
 
 -- --------------------------------------------------------
@@ -94,7 +94,7 @@ CREATE TABLE `book_order` (
   `order_subtotal_amount` double NOT NULL,
   `order_total_amount` double NOT NULL,
   `special_instruction` varchar(200) DEFAULT NULL,
-  `payment_method` int(11) DEFAULT NULL,
+  `payment_method` varchar(50) DEFAULT NULL,
   `shipping_method` int(11) DEFAULT NULL,
   `shipping_street` varchar(80) DEFAULT NULL,
   `shipping_apartment` varchar(30) DEFAULT NULL,
@@ -109,10 +109,10 @@ CREATE TABLE `book_order` (
 --
 
 INSERT INTO `book_order` (`id`, `user_id`, `order_date`, `order_status`, `transaction_id`, `transaction_status`, `order_subtotal_amount`, `order_total_amount`, `special_instruction`, `payment_method`, `shipping_method`, `shipping_street`, `shipping_apartment`, `shipping_province`, `shipping_city`, `shipping_postcode`, `shipping_country`) VALUES
-('EuZZtdMsGp', 5, '2022-09-12 00:00:00', 1, NULL, 1, 1000, 1000, '', NULL, 1, 'shantinagar', '', '', 'kathmandu', 3440062, 'Nepal'),
-('HmKGT4j2t0', 1, '2022-10-19 00:00:00', 1, NULL, 1, 1410, 1410, '', NULL, 1, 'rthdth', 'tfhrj', 'vzxfhe56hb', 'adfvaXDV', 3343353, 'Nepal'),
-('s1zwyYIuez', 5, '2022-10-25 00:00:00', 1, NULL, 1, 400, 400, '', NULL, 1, 'qergqergf', 'wergerg', 'rgwrg', 'wergwerg', NULL, 'Nepal'),
-('xCW0LetSmA', 1, '2022-10-22 00:00:00', 1, NULL, 1, 1295, 1295, '', NULL, 1, 'dfsvsdfdgbhfgebgfn', 'dfvwfdbvwrfvwrv', 'dsfvsdfvwfv', 'wdfvwfv', 123322, 'Nepal');
+('aUdOReX4nB', 18, '2022-11-05 00:00:00', 1, NULL, 1, 879, 993, '', 'Cash On Delivery', 1, 'siddhartha marga', 'kapan-07', 'Bagmati', 'Kathmandu', NULL, 'Nepal'),
+('HYfDcEX16w', 1, '2022-11-01 00:00:00', 1, NULL, 1, 2041, 2306, 'fyfff sthvhvhhh', 'Cash On Delivery', 1, 'Simrik marga, Buddhanagar', 'New baneshwor', 'Bagmati', 'Kathmandu', 123322, 'Nepal'),
+('uzXF5V3qE2', 18, '2022-11-05 00:00:00', 1, NULL, 1, 879, 993, '', 'Cash On Delivery', 1, 'siddhartha marga', 'kapan-07', 'Bagmati', 'Kathmandu', NULL, 'Nepal'),
+('zvcTbWGvzZ', 1, '2022-10-31 00:00:00', 1, NULL, 1, 1220, 1379, 'please deliver ASAP!!', 'Cash On Delivery', 1, 'Bigyan marga', 'Sankhamul, New baneshwor', 'Bagmati', 'Kathmandu', NULL, 'Nepal');
 
 -- --------------------------------------------------------
 
@@ -156,7 +156,10 @@ INSERT INTO `cart` (`id`, `user_id`, `book_id`, `quantity`, `created_date`) VALU
 (54, 9, 5, 1, '2022-09-13'),
 (55, 9, 4, 2, '2022-09-13'),
 (56, 9, 7, 1, '2022-09-13'),
-(83, 1, 9, 1, '2022-10-26');
+(86, 1, 5, 3, '2022-10-27'),
+(87, 5, 16, 4, '2022-10-28'),
+(89, 1, 7, 1, '2022-10-29'),
+(91, 3, 4, 1, '2022-11-01');
 
 -- --------------------------------------------------------
 
@@ -180,7 +183,8 @@ INSERT INTO `categories` (`id`, `category_name`) VALUES
 (6, 'Motivation'),
 (8, 'Finance'),
 (9, 'Romance'),
-(11, 'Sports');
+(11, 'Sports'),
+(29, 'Mystery and horror');
 
 -- --------------------------------------------------------
 
@@ -193,6 +197,15 @@ CREATE TABLE `city` (
   `city_name` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `city`
+--
+
+INSERT INTO `city` (`id`, `city_name`) VALUES
+(1, 'Kathmandu'),
+(2, 'Bhaktapur'),
+(3, 'Lalitpur');
+
 -- --------------------------------------------------------
 
 --
@@ -203,6 +216,13 @@ CREATE TABLE `country` (
   `id` int(11) NOT NULL,
   `country_name` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `country`
+--
+
+INSERT INTO `country` (`id`, `country_name`) VALUES
+(1, 'Nepal');
 
 -- --------------------------------------------------------
 
@@ -250,13 +270,16 @@ CREATE TABLE `order_items` (
 --
 
 INSERT INTO `order_items` (`id`, `order_id`, `book_id`, `quantity`, `unit_price`, `total_price`, `tax_amount`, `shipping_amount`) VALUES
-(2, 'EuZZtdMsGp', 4, 1, 0, 1000, 0, 0),
-(10, 'HmKGT4j2t0', 4, 2, 0, 1410, 0, 0),
-(11, 'HmKGT4j2t0', 5, 1, 0, 1410, 0, 0),
-(12, 'xCW0LetSmA', 7, 1, 0, 1295, 0, 0),
-(13, 'xCW0LetSmA', 8, 1, 0, 1295, 0, 0),
-(14, 'xCW0LetSmA', 16, 1, 0, 1295, 0, 0),
-(15, 's1zwyYIuez', 7, 1, 0, 400, 0, 0);
+(28, 'zvcTbWGvzZ', 5, 2, 410, 820, 107, 0),
+(29, 'zvcTbWGvzZ', 7, 1, 400, 400, 52, 0),
+(30, 'HYfDcEX16w', 5, 3, 410, 1230, 160, 0),
+(31, 'HYfDcEX16w', 7, 1, 400, 400, 52, 0),
+(32, 'HYfDcEX16w', 16, 1, 345, 345, 45, 0),
+(33, 'HYfDcEX16w', 4, 2, 33, 66, 9, 0),
+(34, 'aUdOReX4nB', 4, 1, 479, 479, 62, 0),
+(35, 'aUdOReX4nB', 7, 1, 400, 400, 52, 0),
+(36, 'uzXF5V3qE2', 4, 1, 479, 479, 62, 0),
+(37, 'uzXF5V3qE2', 7, 1, 400, 400, 52, 0);
 
 -- --------------------------------------------------------
 
@@ -293,6 +316,13 @@ CREATE TABLE `payment_method` (
   `is_default` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `payment_method`
+--
+
+INSERT INTO `payment_method` (`id`, `user_id`, `payment_type_id`, `provider`, `account_number`, `expiry_date`, `is_default`) VALUES
+(1, 5, 1, NULL, NULL, NULL, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -301,7 +331,7 @@ CREATE TABLE `payment_method` (
 
 CREATE TABLE `payment_type` (
   `id` int(11) NOT NULL,
-  `payment_type` varchar(20) NOT NULL
+  `payment_type` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -309,7 +339,9 @@ CREATE TABLE `payment_type` (
 --
 
 INSERT INTO `payment_type` (`id`, `payment_type`) VALUES
-(1, 'Cash On Delivery');
+(1, 'Cash On Delivery'),
+(2, 'Digital payment (eSewa)'),
+(3, 'Credit card (Master/Visa/JCB/American express)');
 
 -- --------------------------------------------------------
 
@@ -322,6 +354,13 @@ CREATE TABLE `province` (
   `province_name` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `province`
+--
+
+INSERT INTO `province` (`id`, `province_name`) VALUES
+(1, 'Bagmati');
+
 -- --------------------------------------------------------
 
 --
@@ -332,13 +371,23 @@ CREATE TABLE `shipping_address` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `street` varchar(100) NOT NULL,
-  `apartment` varchar(50) NOT NULL,
+  `apartment` varchar(50) DEFAULT NULL,
   `city` int(11) NOT NULL,
   `province` int(11) NOT NULL,
-  `postal_code` int(11) NOT NULL,
+  `postal_code` int(11) DEFAULT NULL,
   `country` int(11) NOT NULL,
   `is_default` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `shipping_address`
+--
+
+INSERT INTO `shipping_address` (`id`, `user_id`, `street`, `apartment`, `city`, `province`, `postal_code`, `country`, `is_default`) VALUES
+(1, 1, 'Simrik marga, Buddhanagar', 'New baneshwor', 1, 1, 123322, 1, 1),
+(20, 2, 'dfgsrthg tgwrth uolioliiu olololo', 'New baneshwor gaa', 1, 1, 1011, 1, 0),
+(25, 18, 'bindabasini marga', 'imaodol-3', 2, 1, NULL, 1, 1),
+(26, 18, 'siddhartha marga', 'kapan-07', 1, 1, NULL, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -408,7 +457,9 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `store_name`, `phone_number`
 (4, NULL, NULL, 'Buddha Books', 3453452222, 'vendor2@gmail.com', 'dmVuZG9yMg==', 'C:\\Users\\Umesh\\OneDrive\\Documents\\NetBeansProjects\\BookStack\\web\\images\\vendor_profiles\\0dd4207f83d869d21b6320d62736b48e-1000.jpg', 'buddha books-profile_pic.jpg', 2),
 (5, 'Simran', 'Acharya', NULL, NULL, 'simran@gmail.com', 'c2ltcmFu', NULL, NULL, 1),
 (8, 'Arjun', 'Sitoula', NULL, 0, 'arjun@gmail.com', 'YXJqdW4=', NULL, NULL, 1),
-(9, 'Prince', 'Singh', NULL, 0, 'prince@gmail.com.np', 'MTIzNDU=', NULL, NULL, 1);
+(9, 'Prince', 'Singh', NULL, 0, 'prince@gmail.com.np', 'MTIzNDU=', NULL, NULL, 1),
+(13, NULL, NULL, 'Pairabi Prakashan PVT. LTD.', 9860452342, 'vendor3@gmail.com', 'dmVuZG9y', 'C:\\Users\\Umesh\\OneDrive\\Documents\\NetBeansProjects\\BookStack\\web\\images\\vendor_profiles\\Pairabi Prakashan-profile_pic.png', 'Pairabi Prakashan-profile_pic.png', 2),
+(18, 'Ramesh', 'Niure Sharma', NULL, 1234567890, 'ramesh@gmail.com', 'cmFtZXNoMQ==', 'C:\\Users\\Umesh\\OneDrive\\Documents\\NetBeansProjects\\BookStack\\web\\images\\user_profiles\\\\Ramesh-Niure Sharma-18.png', 'Ramesh-Niure Sharma-18.png', 1);
 
 -- --------------------------------------------------------
 
@@ -460,7 +511,6 @@ ALTER TABLE `book_order`
   ADD KEY `order_status` (`order_status`),
   ADD KEY `transaction_satus` (`transaction_status`),
   ADD KEY `user_id` (`user_id`),
-  ADD KEY `payment_method` (`payment_method`),
   ADD KEY `shipping_method` (`shipping_method`);
 
 --
@@ -590,43 +640,43 @@ ALTER TABLE `book_cover`
 -- AUTO_INCREMENT for table `book_type`
 --
 ALTER TABLE `book_type`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=84;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `city`
 --
 ALTER TABLE `city`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `country`
 --
 ALTER TABLE `country`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `language`
 --
 ALTER TABLE `language`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `order_items`
 --
 ALTER TABLE `order_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `order_status`
@@ -638,25 +688,25 @@ ALTER TABLE `order_status`
 -- AUTO_INCREMENT for table `payment_method`
 --
 ALTER TABLE `payment_method`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `payment_type`
 --
 ALTER TABLE `payment_type`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `province`
 --
 ALTER TABLE `province`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `shipping_address`
 --
 ALTER TABLE `shipping_address`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `shipping_method`
@@ -674,7 +724,7 @@ ALTER TABLE `transaction_status`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `user_type`
@@ -703,8 +753,7 @@ ALTER TABLE `book_order`
   ADD CONSTRAINT `book_order_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `book_order_ibfk_2` FOREIGN KEY (`transaction_status`) REFERENCES `transaction_status` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
   ADD CONSTRAINT `book_order_ibfk_3` FOREIGN KEY (`order_status`) REFERENCES `order_status` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `book_order_ibfk_4` FOREIGN KEY (`shipping_method`) REFERENCES `shipping_method` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  ADD CONSTRAINT `book_order_ibfk_5` FOREIGN KEY (`payment_method`) REFERENCES `payment_method` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `book_order_ibfk_4` FOREIGN KEY (`shipping_method`) REFERENCES `shipping_method` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- Constraints for table `cart`
@@ -717,8 +766,8 @@ ALTER TABLE `cart`
 -- Constraints for table `order_items`
 --
 ALTER TABLE `order_items`
-  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `book_order` (`id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `book_order` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `payment_method`
